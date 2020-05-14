d98dc8e2baa1febae5d78884e7e7f8ec1288ad05
Rafael Jesus do Prado <rafaeljprado@gmail.com>
2020-05-14 15:14:00 -0300

    subir arquivos

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,8 +6,18 @@ namespace Composer\Autoload;
 
 class ComposerStaticInitc97f07f0134db49ff3b94021e1c981c8
 {
-    public static $fallbackDirsPsr4 = array (
-        0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+    public static $prefixLengthsPsr4 = array (
+        'H' => 
+        array (
+            'Hcode\\' => 6,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Hcode\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+        ),
     );
 
     public static $prefixesPsr0 = array (
@@ -41,7 +51,8 @@ class ComposerStaticInitc97f07f0134db49ff3b94021e1c981c8
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->fallbackDirsPsr4 = ComposerStaticInitc97f07f0134db49ff3b94021e1c981c8::$fallbackDirsPsr4;
+            $loader->prefixLengthsPsr4 = ComposerStaticInitc97f07f0134db49ff3b94021e1c981c8::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitc97f07f0134db49ff3b94021e1c981c8::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInitc97f07f0134db49ff3b94021e1c981c8::$prefixesPsr0;
             $loader->classMap = ComposerStaticInitc97f07f0134db49ff3b94021e1c981c8::$classMap;
 
