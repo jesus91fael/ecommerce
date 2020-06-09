78e7d2189740e1836a8843eb360f49459660d89d
Rafael Jesus do Prado <rafaeljprado@gmail.com>
2020-06-09 10:16:44 -0300

    Criamos classe page

---
+++ views-cache/header.17c94dae260ae9fd369ffa3cdd778314.rtpl.php
@@ -0,0 +1,11 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><!DOCTYPE html>
+
+<html>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Document</title>
+</head>
+
+<body>
